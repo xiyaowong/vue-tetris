6ba197dcccbf484826fc252e5733928022f0f7ec
wongxy <xiyao.wong@foxmail.com>
2021-11-16 13:00:04 +0800

    do not increase speed, for my girlfriend:)

--- src/vuex/mutations.js
+++ src/vuex/mutations.js
@@ -1,77 +1,77 @@
-import { getNextType } from '../unit'
-import Block from '../unit/block'
+import { getNextType } from "../unit";
+import Block from "../unit/block";
 const mutations = {
   nextBlock(state, data) {
     if (!data) {
-      data = getNextType()
+      data = getNextType();
     }
-    state.next = data
+    state.next = data;
   },
   moveBlock(state, data) {
-    state.cur = data.reset === true ? null : new Block(data)
+    state.cur = data.reset === true ? null : new Block(data);
   },
   speedStart(state, data) {
-    state.speedStart = data
+    state.speedStart = data;
   },
   speedRun(state, data) {
-    state.speedRun = data
+    state.speedRun = state.speedRun;
   },
   startLines(state, data) {
-    state.startLines = data
+    state.startLines = data;
   },
   matrix(state, data) {
-    state.matrix = data
+    state.matrix = data;
   },
   lock(state, data) {
-    state.lock = data
+    state.lock = data;
   },
   clearLines(state, data) {
-    state.clearLines = data
+    state.clearLines = data;
   },
   points(state, data) {
-    state.points = data
+    state.points = data;
   },
   max(state, data) {
-    state.max = data
+    state.max = data;
   },
   reset(state, data) {
-    state.reset = data
+    state.reset = data;
   },
   drop(state, data) {
-    state.drop = data
+    state.drop = data;
   },
   pause(state, data) {
-    state.pause = data
+    state.pause = data;
   },
   music(state, data) {
-    state.music = data
+    state.music = data;
   },
   focus(state, data) {
-    state.focus = data
+    state.focus = data;
   },
   key_drop(state, data) {
-    state.keyboard['drop'] = data
+    state.keyboard["drop"] = data;
   },
   key_down(state, data) {
-    state.keyboard['down'] = data
+    state.keyboard["down"] = data;
   },
   key_left(state, data) {
-    state.keyboard['left'] = data
+    state.keyboard["left"] = data;
   },
   key_right(state, data) {
-    state.keyboard['right'] = data
+    state.keyboard["right"] = data;
   },
   key_rotate(state, data) {
-    state.keyboard['rotate'] = data
+    state.keyboard["rotate"] = data;
   },
   key_reset(state, data) {
-    state.keyboard['reset'] = data
+    state.keyboard["reset"] = data;
   },
   key_music(state, data) {
-    state.keyboard['music'] = data
+    state.keyboard["music"] = data;
   },
   key_pause(state, data) {
-    state.keyboard['pause'] = data
-  }
-}
-export default mutations
+    state.keyboard["pause"] = data;
+  },
+};
+export default mutations;
